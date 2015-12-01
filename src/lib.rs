a620cb51caa6424723819d0452b1f2d1da7cc0df
Josiah Gaskin <josiah@optimizely.com>
2015-11-30 19:17:28 -0800

    Got everything running

--- src/lib.rs
+++ src/lib.rs
@@ -10,20 +10,26 @@ enum Change {
     Replace(Vec<String>, json::Json),
 }
 
-impl fmt::Debug for Change {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Change {
+    fn write(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            &Change::Remove(ref kp, ref j) => write!(f, "Remove()"),
-            &Change::Add(ref kp, ref j) => write!(f, "Add()"),
-            &Change::Insert(ref kp, ref j) => write!(f, "Insert()"),
-            &Change::Rename(ref kp, ref j) => write!(f, "Rename()"),
-            &Change::Replace(ref kp, ref j) => write!(f, "Replace()"),
+            &Change::Remove(ref kp, ref j) => write!(f, "Remove({})", j),
+            &Change::Add(ref kp, ref j) => write!(f, "Add({})", j),
+            &Change::Insert(ref kp, ref j) => write!(f, "Insert({})", j),
+            &Change::Rename(ref kp, ref j) => write!(f, "Rename({})", j),
+            &Change::Replace(ref kp, ref j) => write!(f, "Replace({})", j),
         }
     }
 }
 
+impl fmt::Debug for Change {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.write(f)
+    }
+}
+
 fn json_diff(left: &json::Json, right: &json::Json) -> Vec<Change> {
-    Vec::new()
+    vec!(Change::Remove(Vec::new(), json::Json::Null))
 }
 
 
@@ -32,5 +38,5 @@ fn it_works() {
     let left = json::Json::from_str("{\"a\": 100, \"b\": [1, 2, 3]}").unwrap();
     let right = json::Json::from_str("{\"a\": 100, \"b\": [1, 2, 3, 4]}").unwrap();
     let changes = json_diff(&left, &right);
-    assert_eq!(0, changes.len());
+    assert_eq!("[Remove(null)]", format!("{:?}", changes));
 }
